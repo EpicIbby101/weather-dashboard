4777b594b483dba03f77e2c869aca393d167d528
EpicIbby101 <kevin_grittner@outlook.com>
2023-09-19 15:29:51 +0100

    Added some css and js logic

--- script.js
+++ script.js
@@ -13,3 +13,43 @@
 // 5 DAY FORECAST
 // For the selected city, I'll also need to present the weather forecast for the following 5 days
 // This follows a similar approach to getting the current weather forecast but I'm calling on different API data
+
+function fetchWeatherData(city) {
+    const apiKey = 'aa57fc75af5a470d19c3d65bd59460fa';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    return fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => {
+            console.error('Error fetching weather data:', error);
+            return null;
+        });
+}
+
+$('#search-form').submit(function (event) {
+    event.preventDefault();
+
+    const city = $('#search-input').val();
+
+    if (city) {
+        fetchWeatherData(city)
+            .then((data) => {
+                if (data) {
+                    // Display weather data in the list
+                    const weatherInfo = `
+                        <div class="list-group-item">
+                            <h4>${data.name}, ${data.sys.country}</h4>
+                            <p>Temperature: ${data.main.temp}°C</p>
+                            <p>Weather: ${data.weather[0].description}</p>
+                            <p>Humidity: ${data.main.humidity}%</p>
+                            <p>Wind Speed: ${data.wind.speed} m/s</p>
+                        </div>
+                    `;
+
+                    $('#history').prepend(weatherInfo);
+                }
+            });
+    }
+});
+
