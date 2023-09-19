0c1c90096edd1025c4dbb5cbdce39f59ba24b6a9
EpicIbby101 <kevin_grittner@outlook.com>
2023-09-19 15:04:01 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// PSEUDOCODE FOR WEATHER APP
+// 
+// HEADER COMPONENT
+// I should have a simple header that says 'Weather Dashboard' against a coloured background
+//
+// CITY SELECTOR
+// I should have a search bar that allows me to search up a city to find the weather forecast
+// Beneath the search bar I could have a search history of recently searched cities that is added to whenever a city is searched
+//
+// CURRENT WEATHER FORECAST
+// I need to create a component that calls on the weather API and populates the element with the relevant information and print it on the screen
+//
+// 5 DAY FORECAST
+// For the selected city, I'll also need to present the weather forecast for the following 5 days
+// This follows a similar approach to getting the current weather forecast but I'm calling on different API data
